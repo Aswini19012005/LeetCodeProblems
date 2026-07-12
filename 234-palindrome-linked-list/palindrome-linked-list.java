0642b8b7af2be266b88c22c4f63ef3be7ff0a38f
Jeetu <aswinipattanaik06@gmail.com>
2026-07-12 22:52:34 +0530

    Time: 4 ms (66.84%) | Memory: 94.2 MB (90.10%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -7,7 +7,7 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+ *////done
 class Solution {
     public static ListNode reverse(ListNode head){
         ListNode temp=head;
