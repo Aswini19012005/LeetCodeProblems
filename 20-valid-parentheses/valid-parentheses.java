bb1a95a7c324d34426f02d788e87a9b8745cce49
Jeetu <aswinipattanaik06@gmail.com>
2026-07-20 23:47:02 +0530

    Time: 3 ms (86.86%) | Memory: 43.5 MB (26.33%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0)return false;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' ||ch=='[' || ch=='{') st.push(ch);
+
+            else{
+                if(st.size()==0) return false;
+                char top=st.peek();
+                if(pattern(top,ch))st.pop();
+                else return false;
+            }
+        } 
+        return (st.size()==0);  
+    }
+    static boolean pattern(char a,char b){
+        if(a=='('&& b==')') return true;
+        if(a=='['&& b==']') return true;
+        if(a=='{'&& b=='}') return true;
+        return false;
+    }
+}
